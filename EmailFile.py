efe550e3ddb31090e045b19737b4cdd9e617248d
odainef@gmail.com <odainef@gmail.com>
2023-10-22 19:01:21 -0400

    more updates to email sending func

--- EmailFile.py
+++ EmailFile.py
@@ -1,26 +1,13 @@
-def the_function():
+def the_function(html):
     import smtplib
 
     # Connect to SMTP server
-<<<<<<< HEAD
-    smtp = smtplib.SMTP('smtp.example.com', 587)
+    smtp = smtplib.SMTP("smtp.gmail.com", 587)
     smtp.ehlo()
-    smtp.starttls() 
+    smtp.starttls()
     smtp.ehlo()
-    smtp.login('username', 'password')
+    smtp.login("weeklythreeoneone", "abcde1234!")  # Username and PW
 
-    # Send email  
-    smtp.sendmail('from@email.com', 'to@email.com', html) 
-=======
-    smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp.ehlo()
-    smtp.starttls() 
-    smtp.ehlo()
-    smtp.login('weeklythreeoneone@gmail.com', 'abcde1234!') #Username and PW 
-
-    # Send email  
-    smtp.sendmail('weeklythreeoneone@gmail.com', 'andrew.dabinett@gmail.com', html) 
->>>>>>> d484a9a944386d4cf60831173acfbe6c825d4ad9
+    # Send email
+    smtp.sendmail("weeklythreeoneone@gmail.com", "andrew.dabinett@gmail.com", html)
     smtp.quit()
-
-the_function()
