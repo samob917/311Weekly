dad422fb9439cbff92cdf0fc0bdc29da7dd1178c
samob917 <Soberly1@jhu.edu>
2023-10-22 16:25:16 -0400

    function to get answers from table

--- hugging_face_model.py
+++ hugging_face_model.py
@@ -6,19 +6,12 @@ headers = {"Authorization": "Bearer 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"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
-output = query({
-	"inputs": {
-		"query": "How many stars does the transformers repository have?",
-		"table": {
-			"Repository": ["Transformers", "Datasets", "Tokenizers"],
-			"Stars": ["36542", "4512", "3934"],
-			"Contributors": ["651", "77", "34"],
-			"Programming language": [
-				"Python",
-				"Python",
-				"Rust, Python and NodeJS"
-			]
-		}
-	},
-})
+
+def get_table_answer(question, df):
+    output = query({
+        "inputs": {
+            "query": f"{question}",
+            "table": f"{df}"        
+        },
+    })
+    return output
