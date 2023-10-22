ca4da9678eb2795f93a4a568aaa20c283016fcea
samob917 <Soberly1@jhu.edu>
2023-10-22 16:23:26 -0400

    hf model

---
+++ hugging_face_model.py
@@ -0,0 +1,24 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/microsoft/tapex-base-finetuned-wikisql"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": {
+		"query": "How many stars does the transformers repository have?",
+		"table": {
+			"Repository": ["Transformers", "Datasets", "Tokenizers"],
+			"Stars": ["36542", "4512", "3934"],
+			"Contributors": ["651", "77", "34"],
+			"Programming language": [
+				"Python",
+				"Python",
+				"Rust, Python and NodeJS"
+			]
+		}
+	},
+})
